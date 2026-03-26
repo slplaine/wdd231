c63514709a4221000efecd932cfa5d3f663ee4be
slplaine <simoneleca@gmail.com>
2026-03-26 16:59:12 -0300

    making learning activities

--- week03/scripts/course.mjs
+++ week03/scripts/course.mjs
@@ -16,7 +16,21 @@ const byuiCourse = {
       sectionNumber: 3,
       enrolled: 95,
       instructor: "Sister Smith",
-    }
+    },
   ],
+  changeEnrollment: function (sectionNumber, add = true) {
+    // Find the section with the given section number
+    const sectionIndex = this.sections.findIndex(
+      (section) => section.sectionNumber == sectionNumber
+    );
+    if (sectionIndex >= 0) {
+      if (add) {
+        this.sections[sectionIndex].enrolled++;
+      } else {
+        this.sections[sectionIndex].enrolled--;
+      }
+      // renderSections(this.sections);
+    }
+  },
 };
 export default byuiCourse;
